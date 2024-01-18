3246e8e069d63c739c81c765126090f1b3ce7e8d
pranav-suryawanshi <156771729+pranav-suryawanshi@users.noreply.github.com>
2024-01-18 14:30:21 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".type-auto",{
+    strings:["Frontend Developer", "Java Programmer", "Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
